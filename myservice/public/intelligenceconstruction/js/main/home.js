67584ad45e5a13bdfce0e54ae0093d835a98c5bf
cohen8888 <liwei012_2000@126.com>
2018-11-07 20:46:53 +0800

    项目初始化

---
+++ myservice/public/intelligenceconstruction/js/main/home.js
@@ -0,0 +1,27 @@
+$(function(){
+
+    var btn_cc = 'btn-primary';
+    var navbar_cc = 'cm-navbar-primary';
+	var pageArr = ["html/home.html", "", "html/projectprogress4global.html", "html/projectprogress4milestone.html"]
+
+
+	$("#cm-menu-scroller a").on("click", function(event){
+		let e = window.event || event;
+		$("#cm-menu-scroller a").each((index, elem) =>{
+			$(elem).removeClass('selected');
+		})
+		$(e.target).addClass('selected')
+
+		// if($(e.target).parent().attr('class') == 'cm-submenu open' ){
+			
+		// }
+
+		if(pageArr[$("#cm-menu-scroller a").index(e.target)] != ''){
+			$("#global .container-fluid").load(pageArr[$("#cm-menu-scroller a").index(e.target)]);
+		}
+		
+	})
+
+	$("#global .container-fluid").load("./html/home.html");
+
+});
